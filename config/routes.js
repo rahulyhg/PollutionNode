ae0392a0bff4d48bedb4dde45f7635564cc82090
mahesh maurya <mahesh@wohlig.com>
2015-09-12 10:55:42 +0530

    changes

--- config/routes.js
+++ config/routes.js
@@ -22,26 +22,73 @@
 
 module.exports.routes = {
 
-    /***************************************************************************
-     *                                                                          *
-     * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-     * etc. depending on your default view engine) your home page.              *
-     *                                                                          *
-     * (Alternatively, remove this and add an `index.html` file in your         *
-     * `assets` directory)                                                      *
-     *                                                                          *
-     ***************************************************************************/
+        /***************************************************************************
+         *                                                                          *
+         * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
+         * etc. depending on your default view engine) your home page.              *
+         *                                                                          *
+         * (Alternatively, remove this and add an `index.html` file in your         *
+         * `assets` directory)                                                      *
+         *                                                                          *
+         ***************************************************************************/
 
-    '/:galleryid': 'UserController.card',
-    '/': 'UserController.redirect',
-    /***************************************************************************
-     *                                                                          *
-     * Custom routes here...                                                    *
-     *                                                                          *
-     * If a request to a URL doesn't match any of the custom routes above, it   *
-     * is matched against Sails route blueprints. See `config/blueprints.js`    *
-     * for configuration options and examples.                                  *
-     *                                                                          *
-     ***************************************************************************/
+        '/:galleryid': 'UserController.card',
+        '/': 'UserController.redirect',
 
-};
+
+        {
+            "likes": {
+                "data": [],
+                "summary": {
+                    "total_count": 0,
+                    "can_like": true,
+                    "has_liked": false
+                }
+            },
+            "comments": {
+                "data": [],
+                "summar
+                y ":{"
+                order ":"
+                chronological ","
+                total_count ":0,"
+                can_comment ":true}},"
+                id ":"
+                953826784673418 _953868688002561 "} {
+                "likes": {
+                    "data": [{
+                        "id": "10153588968606585"
+            }, {
+                        "id": "878242832261803"
+            }],
+                    "paging": {
+                        "cursors": {
+                            "after": "ODc4MjQyODM
+                            yMjYxODAz ","
+                            before ":"
+                            MTAxNTM1ODg5Njg2MDY1ODU = "}},"
+                            summary ":{"
+                            total_count ":2,"
+                            can_like ":true,"
+                            has_liked ":true}},"
+                            comments ":{"
+                            data ":[],"
+                            summary ":{"
+                            order ":"
+                            chronological ","
+                            total_count ":0,"
+                            can_comment ":true}},"
+                            id ":"
+                            8782428322618
+                            03 _879536118799141 "}
+                            /***************************************************************************
+                             *                                                                          *
+                             * Custom routes here...                                                    *
+                             *                                                                          *
+                             * If a request to a URL doesn't match any of the custom routes above, it   *
+                             * is matched against Sails route blueprints. See `config/blueprints.js`    *
+                             * for configuration options and examples.                                  *
+                             *                                                                          *
+                             ***************************************************************************/
+
+                        };
