b9971b4a90f5566606609d7acbd791c7a78ddfea
vigkast <vigwohlig@gmail.com>
2015-09-14 16:45:28 +0530

    console result

--- api/models/User.js
+++ api/models/User.js
@@ -516,7 +516,7 @@ module.exports = {
                     if (err) {
                         console.log(err);
                         callback(err, null);
-                    } else if (data2 && data2.length > 0 && data2[0].post && data2[0].post.length > 0 ) {
+                    } else if (data2 && data2.length > 0 && data2[0].post && data2[0].post.length > 0) {
                         callback({
                             value: false,
                             comment: "wait"
@@ -525,6 +525,7 @@ module.exports = {
                         db.collection('user').find({
                             "_id": sails.ObjectID(userid)
                         }).toArray(function (err, result) {
+                            console.log(result);
                             if (err) {
                                 console.log(err);
                                 callback({
